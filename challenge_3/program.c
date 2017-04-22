0a0b44cb25fec61be23c524c62d4ada6a11ab55a
Lionel Blatter <Lionel.BLATTER@cea.fr>
2017-04-22 15:36:57 +0200

    Modifications qui seront validées : 	modifié:         program.c

--- challenge_3/program.c
+++ challenge_3/program.c
@@ -1,18 +1,3 @@
-/*@ requires 0 <= i;
-    requires 0 <= j;
-    requires \valid(list+i);
-    requires \valid(list+j);
-    ensures \forall integer k; k != i && k != j ==> list[k] == \old(list[k]);
-    ensures list[j]==\old(list[i]);
-    ensures list[i]==\old(list[j]);
-    assigns list[i],list[j];
-*/
-void swap (int list[], int i, int j) {
-  int temp = list[i];
-  list[i] = list[j];
-  list[j] = temp;
-}
-
 /*@ predicate swap{L1, L2}(int* a, integer i, integer j, integer length) =
       0 <= i < j < length &&
       \at(a[i], L1) == \at(a[j], L2) &&
@@ -32,6 +17,23 @@ void swap (int list[], int i, int j) {
         same_elements{L1, L3}(a, length);
     }
 */
+
+/*@ requires 0 <= i;
+    requires 0 <= j;
+    requires n >= 0;
+    requires i < j;
+    requires \valid(list+(0..n-1));
+    ensures \forall integer k; k != i && k != j && 0 <= k < n ==> list[k] == \old(list[k]);
+    ensures list[j]==\old(list[i]);
+    ensures list[i]==\old(list[j]);
+    ensures same_elements{Pre, Here}(list, n);
+    assigns list[i],list[j];
+*/
+void swap (int list[], int i, int j,int n) {
+  int temp = list[i];
+  list[i] = list[j];
+  list[j] = temp;
+}
 
 /*@ requires n >= 0;
     requires \valid(list+(0..n-1));
@@ -55,7 +57,7 @@ void oddEvenSort (int list[], int n) {
     */
     for(int i = 1; i < n-1; i+=2) {
       if (list[i] > list[i+1]) {
-        swap(list, i, i+1);
+        swap(list, i, i+1,n);
         sorted = 0;
       }
     }
@@ -67,7 +69,7 @@ void oddEvenSort (int list[], int n) {
     */
     for(int i = 0; i < n-1; i+=2) {
       if (list[i] > list[i+1]) {
-        swap(list, i, i+1);
+        swap(list, i, i+1,n);
         sorted = 0;
       }
     }
