65798e8668b3dde4ede2fab70a3ad600462b17a4
Jean-Christophe Léchenet <jean-christophe.lechenet@cea.fr>
2018-04-14 23:20:05 +0300

    Challenge 1: new version

--- 2018/challenge_1/gap_grow.c
+++ 2018/challenge_1/gap_grow.c
@@ -3,7 +3,7 @@
 char a[MAX];
 int l, r, n;
 int const K;
-//@ global  invariant K_is_positive: K > 0 ;
+// global invariant K_is_positive: K > 0 ;
 
 /*@ axiomatic nth {
   logic int nth(integer i) = i < l ? a[i] : a[i+r-l];
@@ -36,32 +36,42 @@ void right()
   }
 }
 
-/*@ requires 0 <= l && r + K < n;
+/*@ requires 0 <= l < r + K < n <= 800;
+    requires K > 0;
     requires l == r;
     ensures \forall integer i; 0 <= i < n - (r - l) ==> nth(i) == \at(nth(i), Pre);
     ensures l < r;
-    assigns a[r+K..n+K-1];
+    assigns r, n, a[(r+K)..(n+K-1)];
 */
 void grow()
 {
   n = n + K;
 
-  /*@ loop invariant r + K <= i <= n;
-      loop invariant \forall integer j; 0 <= j < r+K ==> a[j] == \at(a[j], LoopEntry);
-      loop invariant \forall integer j; r +K <= j < n ==> a[j] == \at(a[j-K], LoopEntry);
+  /*@ loop invariant r + K - 1 <= i <= n - 1;
+      loop invariant \forall integer j; 0 <= j <= i ==> a[j] == \at(a[j], LoopEntry);
+      loop invariant \forall integer j; i < j < n ==> a[j] == \at(a[j-K], LoopEntry);
       loop assigns i, a[r+K..n+K-1];
   */
-  for (int i = r+K; i < n; i++) {
+  for (int i = n-1; i >= r+K; i--) {
     a[i] = a[i - K];
   }
   r = r + K;
 }
 
 /*@ requires 0 <= l <= r < n;
+    requires r + K < n;
+    requires K > 0;
     ensures \forall integer i; 0 <= i < \old(l) ==> nth(i) == \old(nth(i));
     ensures nth(\old(l)) == c;
     ensures \forall integer i; \old(l)+1 <= i < n - (r-l) ==> nth(i) == \old(nth(i-1));
-    assigns l, a[l];
+    behavior insert:
+      assumes l == r;
+      assigns l, r, n, a[l], a[r+K..n+K-1];
+    behavior other:
+      assumes l < r;
+      assigns l, a[l];
+    complete behaviors;
+    disjoint behaviors;
 */
 void insert (char c)
 {
@@ -82,6 +92,8 @@ void insert (char c)
       assumes l == 0;
       ensures \forall integer i; 0 <= i < n-(r-l) ==> nth(i) == \old(nth(i));
       assigns \nothing;
+    complete behaviors;
+    disjoint behaviors;
 */
 void delete () {
   if (l != 0) {
