e015b66109584bfad4b7c2916c142381bc51cf28
Jean-Christophe Léchenet <jean-christophe.lechenet@inria.fr>
2021-03-26 21:47:26 +0100

    Challenge 2

--- 2021/challenge_2/bst.v
+++ 2021/challenge_2/bst.v
@@ -31,6 +31,7 @@ Definition dll_to_bst (l : list A) :=
   root.
 
 Variable R : A -> A -> Prop.
+Hypothesis R_trans : RelationClasses.Transitive R.
 
 Inductive Forall_tree (P : A -> Prop) : tree -> Prop :=
 | Forall_leaf : Forall_tree P Leaf
@@ -66,27 +67,35 @@ Lemma Forall_tree_impl (P1 P2 : A -> Prop) :
   forall t,
   Forall_tree P1 t ->
   Forall_tree P2 t.
+Proof.
+  intros Himpl t HForall1.
+  induction HForall1.
+  - constructor.
+  - constructor; auto.
+Qed.
+
+Lemma Sorted_app_r l1 l2 :
+  Sorted R (l1 ++ l2) -> Sorted R l2.
+Proof.
+  intros H.
+  induction l1.
+  - assumption.
+  - apply IHl1. inversion H; assumption.
+Qed.
 
 Lemma sorted_dll_to_bst_rec l n : Sorted R l ->
   n <= length l ->
   let (t, l2) := dll_to_bst_rec l n in
   is_bst t /\
-  Forall_tree (fun x => R (hd dflt l) x) t /\
-  Forall_tree (fun x => R x (hd dflt l2)) t /\
-  Sorted R l2. (*
-(*  exists l1, Forall_tree (fun x => In x l1) t /\ l = l1 ++ l2. *)
-  match l2 with
-  | [] => True
-  | a :: l2 => Forall_tree (fun x => R a x) t
-  end. *)
+  Forall_tree (fun x => Forall (R x) l2) t /\
+  exists l1, Forall_tree (fun x => In x l1) t /\ l = l1 ++ l2.
 Proof.
   intros Hsorted Hle.
   funelim (dll_to_bst_rec l n).
   - rewrite <- Heqcall.
     split; [constructor|].
     split; [constructor|].
-    split; [constructor|].
-    assumption.
+    exists []; split; constructor.
   - pose proof (Nat.lt_div2 n).
     destruct (dll_to_bst_rec l (Nat.div2 n)) as [left root] eqn:Hrec1.
     specialize (H0 Leaf root).
@@ -94,11 +103,12 @@ Proof.
     rewrite <- Heqcall.
     assert (Nat.div2 n <= length l) as Ha by lia.
     specialize (H Hsorted Ha).
-    destruct H as (H01 & H02 & H03 & H04).
+    destruct H as (H01 & H02 & (l1' & H03 & H04)).
     assert (Sorted R (tl root)) as Ha2.
     { destruct root.
       - constructor.
-      - simpl. inversion H04; assumption.
+      - simpl. rewrite H04 in Hsorted. apply Sorted_app_r in Hsorted.
+        inversion Hsorted; assumption.
     }
     assert (n - Nat.div2 n - 1 <= length (tl root)) as Ha3.
     { pose proof (dll_to_bst_rec_length l (Nat.div2 n)).
@@ -106,81 +116,82 @@ Proof.
       destruct root; simpl in H |- *; lia.
     }
     specialize (H0 Ha2 Ha3).
-    destruct H0 as (H11 & H12 & H13 & H14).
-    split.
+    destruct H0 as (H11 & H12 & l3 & H14 & H15).
+    repeat split.
     + constructor.
       * assumption.
+      * destruct root.
+        -- simpl in Ha3.
+           pose proof (dll_to_bst_rec_length l (Nat.div2 n)).
+           rewrite Hrec1 in H. simpl in H.
+           lia.
+        -- eapply Forall_tree_impl; try apply H02; simpl.
+           intros x ?.
+           inversion H. assumption.
       * assumption.
-      * assumption.
-      * apply Forall_tree_impl.
-    
-    split.
+      * destruct root.
+        -- simpl in Ha3.
+           replace (n - Nat.div2 n - 1) with 0 in Hrec2 by lia.
+           simpl in Hrec2. simp dll_to_bst_rec in Hrec2.
+           simpl in Hrec2. inversion Hrec2. constructor.
+        -- destruct root.
+           ++ simpl in Ha3.
+              replace (n - Nat.div2 n - 1) with 0 in Hrec2 by lia.
+              simpl in Hrec2. simp dll_to_bst_rec in Hrec2.
+              simpl in Hrec2. inversion Hrec2. constructor.
+           ++ simpl. simpl in *.
+              eapply Forall_tree_impl; try apply H14.
+              intros x ?. simpl in H.
+              rewrite H15 in H04.
+              rewrite H04 in Hsorted.
+              apply Sorted_app_r in Hsorted.
+              apply Sorted_StronglySorted in Hsorted; try assumption.
+              inversion Hsorted.
+              rewrite Forall_forall in H4. apply H4.
+              apply in_app_iff. left; assumption.
     + constructor.
-      * apply H; try assumption.
-        lia.
-      * apply H; try assumption.
-        lia.
-      * specialize (H0 Leaf root).
-        rewrite Hrec2 in H0.
-        apply H0.
-        -- admit.
-        -- pose proof (dll_to_bst_rec_length l (Nat.div2 n)).
-           rewrite Hrec1 in H2.
-           destruct root.
-           simpl in H2 |- *. lia.
-           simpl in H2 |- *. lia.
-      * specialize (H0 Leaf root).
-        rewrite Hrec2 in H0.
-        apply H0.
-      
-       pose proof (dll_to_bst_rec_length l (Nat.div2 n)).
-        rewrite Hrec1 in H2.
-        assert (0 < length root) by lia.
-        destruct root; [inversion H3|].
-        simpl.
-        
-        
-        le_lt_dec
-        lia.
-      *
-Abort.
+      * admit.
+      * destruct root.
+        -- simpl in Ha3.
+           replace (n - Nat.div2 n - 1) with 0 in Hrec2 by lia.
+           simpl in Hrec2. simp dll_to_bst_rec in Hrec2.
+           simpl in Hrec2. inversion Hrec2. constructor.
+        -- simpl. simpl in H15. rewrite H15 in H04.
+           rewrite H04 in Hsorted.
+           apply Sorted_app_r in Hsorted.
+           apply Sorted_StronglySorted in Hsorted; try assumption.
+           inversion Hsorted.
+           apply Forall_app in H3. apply H3.
+     * assumption.
+    + destruct root.
+      -- simpl in Ha3.
+         pose proof (dll_to_bst_rec_length l (Nat.div2 n)).
+         rewrite Hrec1 in H. simpl in H.
+         lia.
+      -- simpl in H15. exists (l1' ++ a :: l3).
+         split.
+         ++ admit.
+         ++ rewrite app_assoc_reverse. simpl.
+            rewrite <- H15. assumption.
+Admitted.
+
+(* Main theorem *)
+Lemma sorted_bst l : Sorted R l -> is_bst (dll_to_bst l).
+Proof.
+  intros Hsorted.
+  unfold dll_to_bst.
+  pose proof (sorted_dll_to_bst_rec l (size l)).
+  destruct (dll_to_bst_rec l (size l)) as [t l2].
+  apply H.
+  assumption.
+  reflexivity.
+Qed.
 
 End test.
-(*
-Lemma sorted_bst {A} (a:A) (R : A -> A -> Prop) l : Sorted R l ->
-  is_bst _ R (dll_to_bst _ a l).
-Proof.
-  unfold dll_to_bst.
-  remember (size A l) as n.
-  assert (n >= size A l).
-  { lia. }
-  clear Heqn.
-  funelim (dll_to_bst_rec A a l n).
-  - intros.
-  rewrite <- Heqcall. constructor.
-  - rewrite <- Heqcall.
-*)
 
+(*
 Goal dll_to_bst_rec _ 0 [1;2;3;4;5] 10 = (Leaf _, []).
 Proof.
   repeat (simp dll_to_bst_rec; simpl).
   simp dll_to_bst_rec. simpl.
-
-Function dll_to_bst_rec (l : list A) n {measure (fun n => n) n} : tree * list A :=
-  if 0 <? n then
-    let (left, root) := dll_to_bst_rec l (Nat.div2 n) in
-    let (temp, right) := dll_to_bst_rec (tl root) (n - Nat.div2 n - 1) in
-    (Node left (hd dflt root) temp, right)
-  else
-    (Leaf, l).
-Proof.
-  - intros _ n H _ _ _ _.
-    apply Nat.ltb_lt in H.
-    lia.
-  - intros _ n H. apply Nat.ltb_lt in H.
-    apply Nat.lt_div2; assumption.
-Defined.
-
-End test.
-
-Eval compute in dll_to_bst_rec _ 0 [1;2;3;4;5] 3.
+*)
