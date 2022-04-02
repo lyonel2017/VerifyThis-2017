3a7d99653c63f0ad20cad3568adca5c2306c83c1
Jean-Christophe Léchenet <jean-christophe.lechenet@inria.fr>
2022-04-02 14:47:45 +0200

    Challenge 2

--- 2022/challenge_2/challenge_2.c
+++ 2022/challenge_2/challenge_2.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #define N 1000
 
 #define empty_array {{0}, 0}
@@ -7,26 +9,45 @@ typedef struct array {
   int index;
 } array;
 
+/*@ logic integer length (array a) = a.index;
+    logic int get (array a, int i) = a.a[i];
+*/
+
 typedef struct sr {
   array runs;
   array data;
 } sr;
 
+/*@ requires 0 <= i < a.index <= N;
+    terminates \true;
+    ensures \result == a.a[i];
+*/
 int get (array a, int i) {
   return a.a[i];
 }
 
+/*@ requires 0 <= a.index <= N;
+    terminates \true;
+    ensures \result == a.index;
+*/
 int length (array a) {
   return a.index;
 }
 
+/*@ terminates \true;
+    ensures \result.index == a.index + 1;
+    ensures \result.a[a.index] == x;
+    ensures \forall integer i; 0 <= i < a.index ==> \result.a[i] == a.a[i];
+*/
 array push_back (array a, int x) {
   if (a.index < N) {
     a.a[a.index++] = x;
-  };
+  } else { exit(1); };
   return a;
 }
 
+/*@ terminates \true;
+*/
 sr merge (sr r1, sr r2) {
   sr res = {empty_array, empty_array};
   int di1 = 0;
@@ -34,11 +55,18 @@ sr merge (sr r1, sr r2) {
   int ri1 = 0;
   int ri2 = 0;
 
-  while (ri1 < length (r1.runs) || ri1 < length(r2.runs)) {
+  /*@ loop invariant 0 <= ri1 <= length(r1.runs);
+      loop invariant 0 <= ri2 <= length(r2.runs);
+      loop variant length (r1.runs) + length (r2.runs) - ri1 - ri2;
+  */
+  while (ri1 < length (r1.runs) || ri2 < length(r2.runs)) {
     int t1 = ri1 < length(r1.runs) && (ri2 == length(r2.runs) || get(r1.data,di1) <= get(r2.data,di2));
     int t2 = ri2 < length(r2.runs) && (ri1 == length(r1.runs) || get(r2.data,di2) <= get(r1.data,di1));
 
     if (t1) {
+      /*@ 
+          loop variant get(r1.runs,ri1) - di1;
+      */
       for (; di1 < get(r1.runs,ri1); ++di1) {
         res.data = push_back(res.data, get(r1.data,di1));
       }
@@ -46,6 +74,8 @@ sr merge (sr r1, sr r2) {
     }
 
     if (t2) {
+      /*@ loop variant get(r2.runs,ri2) - di2;
+      */
       for (; di2 < get(r2.runs,ri2); ++di2) {
         res.data = push_back(res.data, get(r2.data,di2));
       }
